a8f01ad44c3aac71a44834ea1e715ae082f926b7
Erick-Simas <erickalgun82@gmail.com>
2022-09-16 22:17:27 -0300

    DOM continuação

---
+++ DOM/ex03.js
@@ -0,0 +1,21 @@
+let e;
+//e = document.head;
+//e = document.body
+//e = document.body.firstElementChild;
+//e = document.body.firstElementChild.firtsElementChild;
+//e = document.doctype;
+//e = document.doctype.name;
+//e = document.forms;
+
+e = document.querySelector('div.row');
+e = document.querySelector('div.container');
+e = document.querySelector('span');
+e = document.querySelector('title');
+e = document.querySelector('link');
+
+
+
+
+
+
+console.log(e);
--- DOM/ex2.js
+++ DOM/ex2.js
@@ -1,15 +1,32 @@
-let btnCalcular = document.getElementById('Calcular');
+let btnCalcular = document.getElementById('calcular');
 
-btnCalcular = addEventListener('click' ,function (evento){
-
+btnCalcular.addEventListener('click', function (evento){
     evento.preventDefault();
+
+    let servico = document.getElementById('servico');
+    let prazo = document.getElementById('prazo');
+    let res = document.getElementById('resultado');
+
+    //recuperar os dados da matriz
+    let preco = calcularPreco(servico.value, prazo.value);
+
+    //mostra o resultado no elemento
+    res.innerText = `o preco é ${preco}`
+
+
+    console.log(servico.value);
+
+
+    //ver dados do evento (click) no console
     console.log(evento);
+
+    //obter os valores do servico e prazo
 });
 
-function executa(evento){
-    console.log('Clicou');
+function calcularPreco(servico, prazo){
+        let tabela = [['$$$', '$$', "$"],
+                      ['$$$$', '$$$', "$$"],
+                      ['nao faz', 'nao faz', "$$$"]];
+
+        return tabela[servico][prazo];
 }
-
-console.log(btnCalcular);
-
-
